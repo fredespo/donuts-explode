552ff50b677f4d6931679c85f13f513505d6271b
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-16 23:05:48 -0400

    Add count down timer to bomb

---
+++ Assets/textTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textTimer : MonoBehaviour
+{
+    public float seconds = 10;
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+        if (seconds < 0) seconds = 0;
+        text.text = seconds.ToString("00.00").Replace(".", ":");
+    }
+}
