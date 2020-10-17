5f3cbe51f70effdd7d31d048e8011611b6c75615
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-16 22:33:20 -0400

    Fix start button and rotating bomb looking lopsided

---
+++ Assets/StartButtonListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButtonListener : MonoBehaviour
+{
+    public Button startButton;
+
+    void Start()
+    {
+        Button btn = startButton.GetComponent<Button>();
+        btn.onClick.AddListener(OnStartButtonClicked);
+    }
+
+    void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/sceneTransitioner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTransitioner : MonoBehaviour
+{
+    public GameObject obj;
+
+    public void LoadScene()
+    {
+        Destroy(obj);
+        SceneManager.LoadScene(1);
+    }
+}
