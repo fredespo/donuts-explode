42129a9f1b9ed2f0297b84d019651a72dd943056
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-18 00:29:04 -0400

    Show functional retry button after bomb explodes

---
+++ Assets/GameState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour
+{
+    public void RetryLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/textTimer.cs
+++ Assets/textTimer.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class textTimer : MonoBehaviour
 {
     public Detonator detonator;
+    public GameObject gameOverUI;
+    public float gameOverDelaySec;
     public float seconds = 10;
     private Text text;
 
@@ -22,6 +24,7 @@ public class textTimer : MonoBehaviour
             seconds = 0;
             detonator.activate();
             Destroy(gameObject);
+            gameOverUI.SetActive(true);
         }
         text.text = seconds.ToString("00.00").Replace(".", ":");
     }
