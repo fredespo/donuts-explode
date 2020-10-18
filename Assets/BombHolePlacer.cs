e0bcb25836f068199dddb99ac40cacaeeaa53741
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-18 18:58:01 -0400

    Add triangle-shaped bomb hole

---
+++ Assets/BombHolePlacer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BombHolePlacer : MonoBehaviour
+{
+    public Vector2 center;
+    public float radius;
+    public float degrees;
+
+    void Update()
+    {
+        float radians = degrees * Mathf.Deg2Rad;
+        float x = Mathf.Cos(radians) * radius;
+        float y = Mathf.Sin(radians) * radius;
+        gameObject.transform.position = new Vector3(center.x + x, center.y + y, 0);
+        gameObject.transform.eulerAngles = new Vector3
+        (
+            0,
+            0,
+            degrees + 90
+        );
+    }
+}
