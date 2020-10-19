bbb692a04d16259d0d4c600bec6c445b1d60a4c4
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-18 23:16:40 -0400

    Disable pieces and shooting when the bomb explodes

--- Assets/Detonator.cs
+++ Assets/Detonator.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Detonator : MonoBehaviour
 {
     public GameObject explosion;
+    public GameObject pieces;
+    public GameObject pieceShooter;
 
     public void activate()
     {
         Instantiate(explosion, new Vector3(0, 1.73f, 0), Quaternion.Euler(new Vector3(-90, 0, 0)));
         Destroy(gameObject);
+        pieces.SetActive(false);
+        pieceShooter.SetActive(false);
     }
 }
