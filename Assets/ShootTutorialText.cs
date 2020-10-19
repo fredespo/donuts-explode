1f56c5df7ae0bb22e6e709606908ed6505f8b378
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-18 23:16:21 -0400

    Remove level one tutorial text when the bomb explodes

--- Assets/ShootTutorialText.cs
+++ Assets/ShootTutorialText.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class ShootTutorialText : MonoBehaviour
 {
     public PieceShooter pieceShooter;
+    public GameObject bomb;
 
     void Update()
     {
-        if(pieceShooter.GetPiecesShotCount() > 0)
+        if(pieceShooter.GetPiecesShotCount() > 0 || bomb == null)
         {
             Destroy(gameObject);
         }
