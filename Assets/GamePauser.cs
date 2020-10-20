c0e6d13818357af335e398ebe52f4435cf95654a
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-19 23:03:43 -0400

    Add quit button to pause UI
    It confirms the user really wants to quit before actually going back to the title screen. Also delete level 2 - it will be easier to make other levels by copying level 1 once all major features have been implemented.

--- Assets/GamePauser.cs
+++ Assets/GamePauser.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GamePauser : MonoBehaviour
 {
+    public GameObject tutorialText;
     public GameObject pauseUI;
+    public GameObject pausedButtons;
+    public GameObject quitConfirmUI;
     public GameObject shootTapZone;
     public Rotator bombRotator;
     public textTimer bombTimer;
+    private bool tutTextWasActive;
 
     public void PauseGame()
     {
         pauseUI.SetActive(true);
+        tutTextWasActive = tutorialText.activeSelf;
+        tutorialText.SetActive(false);
         shootTapZone.SetActive(false);
         bombRotator.enabled = false;
         bombTimer.enabled = false;
@@ -21,8 +28,29 @@ public class GamePauser : MonoBehaviour
     public void ResumeGame()
     {
         pauseUI.SetActive(false);
+        if(tutTextWasActive)
+        {
+            tutorialText.SetActive(true);
+        }
         shootTapZone.SetActive(true);
         bombRotator.enabled = true;
         bombTimer.enabled = true;
+    }
+
+    public void ShowConfirmQuitUI()
+    {
+        pausedButtons.SetActive(false);
+        quitConfirmUI.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void CancelQuit()
+    {
+        pausedButtons.SetActive(true);
+        quitConfirmUI.SetActive(false);
     }
 }
