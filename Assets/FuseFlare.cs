24bb95f62db499172056a6a1872cbe94cca44222
Fredrick Esposito <fredrickesposito@gmail.com>
2020-10-20 17:23:30 -0400

    Add flare effect to make it look like fuse is burning

---
+++ Assets/FuseFlare.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseFlare : MonoBehaviour
+{
+    public List<Color> possibleColors;
+    public float fadeSpeed = 1;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        int colorIndex = (int)Mathf.Round(UnityEngine.Random.Range(0, possibleColors.Count - 1));
+        spriteRenderer.color = possibleColors[colorIndex];
+    }
+
+    void Update()
+    {
+        if(spriteRenderer.color.a > 0)
+        {
+            Color color = spriteRenderer.color;
+            color.a -= fadeSpeed * Time.deltaTime;
+            if (color.a < 0)
+            {
+                color.a = 0;
+            }
+            spriteRenderer.color = color;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/FuseFlareSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseFlareSpawner : MonoBehaviour
+{
+    public GameObject flare;
+    public Vector2 spawnDistanceRange;
+    public Vector2 rotationRange;
+    public float spawnDelay = 1.0f;
+    private float spawnTimer = 0.0f;
+
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer >= spawnDelay)
+        {
+            SpawnFlare();
+            spawnTimer = 0.0f;
+        }
+    }
+
+    void SpawnFlare()
+    {
+        GameObject spawnedFlare = Instantiate(flare);
+        spawnedFlare.transform.SetParent(gameObject.transform);
+        float degrees = UnityEngine.Random.Range(rotationRange.x, rotationRange.y);
+        float radians = degrees * Mathf.Deg2Rad;
+        float spawnDistance = UnityEngine.Random.Range(spawnDistanceRange.x, spawnDistanceRange.y);
+        float x = Mathf.Cos(radians) * spawnDistance;
+        float y = Mathf.Sin(radians) * spawnDistance;
+        spawnedFlare.transform.localPosition = new Vector3(x, y, 0);
+        spawnedFlare.transform.localEulerAngles = new Vector3
+        (
+            gameObject.transform.localRotation.x,
+            gameObject.transform.localRotation.y,
+            degrees
+        );
+    }
+}
