bcc5a819980e6bc90b225e5e70412cfabc11071b
Fredrick Esposito <fredrickesposito@gmail.com>
2020-12-21 15:46:35 -0500

    Add back to title button in level won UI

---
+++ Assets/BackToTitleBtn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToTitleBtn : MonoBehaviour
+{
+    private ScreenManager screenManager;
+
+    public void Start()
+    {
+        screenManager = GameObject.FindGameObjectWithTag("ScreenManager").GetComponent<ScreenManager>();
+    }
+
+    public void BackToTitle()
+    {
+        screenManager.ShowTitleScreen();
+    }
+}
