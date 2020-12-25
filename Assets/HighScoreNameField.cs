a7f7f2d717fe337c6fbe72072c5a96bffd94d3f5
Fredrick Esposito <fredrickesposito@gmail.com>
2020-12-25 01:23:54 -0500

    Limit name on high score entry to 11 characters

---
+++ Assets/HighScoreNameField.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreNameField : MonoBehaviour
+{
+    public int maxLen = 10;
+
+    void Start()
+    {
+        GetComponent<InputField>().characterLimit = maxLen;
+    }
+}
