88b3d62d663f4163e2ac31d3be0a22cbad242d24
Fredrick Esposito <fredrickesposito@gmail.com>
2020-12-27 20:19:20 -0500

    Refactor piece shooter related code

--- Assets/PieceShooter.cs
+++ Assets/PieceShooter.cs
@@ -8,30 +8,23 @@ public class PieceShooter : MonoBehaviour
     public bool shootingEnabled = false;
     public GameObject piece;
     public float speed = 1.0f;
-    public float shootDelaySec = 1.0f;
-    private float lastShootTime = 0.0f;
     private GameObject spawnedPiece;
-    private bool showingSpawnedPiece = false;
-    private int piecesShotCount = 0;
     private float minAngle;
     private float maxAngle;
     private Rotator rotator;
 
     public void Init()
     {
-        SpawnPiece();
-        ResetPieceShotCount();
         transform.eulerAngles = new Vector3(0, 0, 0);
     }
 
     void Update()
     {
-        if (spawnedPiece == null && !showingSpawnedPiece)
+        if (spawnedPiece == null)
         {
             SpawnPiece();
         }
-
-        if(showingSpawnedPiece)
+        else
         {
             spawnedPiece.SetActive(shootingEnabled);
             Vector3 pieceRotation = spawnedPiece.transform.eulerAngles;
@@ -61,12 +54,9 @@ public class PieceShooter : MonoBehaviour
 
     public void Shoot()
     {
-        if (shootingEnabled && showingSpawnedPiece)
+        if (shootingEnabled && spawnedPiece != null && spawnedPiece.GetComponent<Rigidbody2D>().velocity.magnitude == 0)
         {
             spawnedPiece.GetComponent<Rigidbody2D>().velocity = transform.up * speed;
-            showingSpawnedPiece = false;
-            ++piecesShotCount;
-            lastShootTime = Time.time;
         }
     }
 
@@ -92,17 +82,6 @@ public class PieceShooter : MonoBehaviour
         spawnedPiece.transform.SetParent(pieceParent.transform);
         spawnedPiece.transform.position = gameObject.transform.position;
         spawnedPiece.transform.localScale = new Vector3(81, 81, 1);
-        showingSpawnedPiece = true;
-    }
-
-    private void ResetPieceShotCount()
-    {
-        piecesShotCount = 0;
-    }
-
-    public int GetPiecesShotCount()
-    {
-        return piecesShotCount;
     }
 
     public void SetShootingEnabled(bool shootingEnabled)
