ad986d486a9fb7cecab67795ff3a6c6a5925391f
Fredrick Esposito <fredrickesposito@gmail.com>
2021-06-03 23:48:06 -0400

    Destroy anything that goes too far off screen

---
+++ Assets/Destroyer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+    }
+}
