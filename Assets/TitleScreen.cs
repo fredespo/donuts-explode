8cf2c5d250369a01f43cb7506b1034892a898ce9
Fredrick Esposito <fredrickesposito@gmail.com>
2021-07-25 21:06:26 -0400

    Make back button go to main menu from new game confirmation

---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public GameObject newGameConfirmationUI;
+
+    public void MinimizeAppIfNotConfirmingNewGame()
+    {
+        if(!ConfirmingNewGame())
+        {
+            MinimizeApp();
+        }
+    }
+
+    private bool ConfirmingNewGame()
+    {
+        return newGameConfirmationUI.activeInHierarchy;
+    }
+
+    private void MinimizeApp()
+    {
+        AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+        activity.Call<bool>("moveTaskToBack", true);
+    }
+}
