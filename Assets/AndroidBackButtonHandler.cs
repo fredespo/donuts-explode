2aa2b61bb68f01c51b22104f206df0c8453c958f
Fredrick Esposito <fredrickesposito@gmail.com>
2021-07-25 20:17:28 -0400

    Handle android back button presses
    Minimize app on title screen. Go back to title screen from game over UI, game win UI, or settings screen.

---
+++ Assets/AndroidBackButtonHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AndroidBackButtonHandler : MonoBehaviour
+{
+    public UnityEvent onBackButtonPressed;
+
+    void Update()
+    {
+        if (BackButtonWasPressedThisFrame())
+        {
+            onBackButtonPressed.Invoke();
+        }
+    }
+
+    public void MinimizeApp()
+    {
+        AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+        activity.Call<bool>("moveTaskToBack", true);
+    }
+
+    private bool BackButtonWasPressedThisFrame()
+    {
+        return Input.GetKeyDown(KeyCode.Escape);
+    }
+}
