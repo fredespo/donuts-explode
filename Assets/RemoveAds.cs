6c3b573f5eb83e98f8372c018217954a8e669749
Fredrick Esposito <fredrickesposito@gmail.com>
2021-08-13 23:23:55 -0400

    Deactivate Remove Ads button if ads are not enabled

--- Assets/RemoveAds.cs
+++ Assets/RemoveAds.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class RemoveAds : MonoBehaviour
 {
+    private Ads ads;
+
+    public void Start()
+    {
+        this.ads = GameObject.FindWithTag("AdsManager").GetComponent<Ads>();
+    }
+
     public void BuyNoAds()
     {
         GameObject.FindWithTag("IAP").GetComponent<IAP>().BuyNoAds();
+    }
+
+    public void Update()
+    {
+        if(!this.ads.adsEnabled)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
