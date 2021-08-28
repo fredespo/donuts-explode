af06e28699c4660aff8896f3512a2738e9062734
Fredrick Esposito <fredrickesposito@gmail.com>
2021-08-28 14:10:20 -0400

    Try to load ad when app is resumed from bg

--- Assets/Ads.cs
+++ Assets/Ads.cs
@@ -17,6 +17,7 @@ public class Ads : MonoBehaviour
     private float origTimeScale;
     private Action<bool> onAdClosed;
     private DataStorage storage;
+    private bool adLoaded = false;
 
     void Start()
     {
@@ -49,7 +50,7 @@ public class Ads : MonoBehaviour
 
     private void LoadInterstitialAd()
     {
-        if(Application.isEditor || !this.adsEnabled)
+        if(Application.isEditor || !this.adsEnabled || this.adLoaded)
         {
             return;
         }
@@ -59,9 +60,11 @@ public class Ads : MonoBehaviour
             this.interstitial.Destroy();
         }
 
+        //https://developers.google.com/admob/unity/interstitial
         this.interstitial = new InterstitialAd(androidAdId);
         this.interstitial.OnAdClosed += HandleOnAdClosed;
         this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
         AdRequest request = new AdRequest.Builder().Build();
         this.interstitial.LoadAd(request);
     }
@@ -74,11 +77,18 @@ public class Ads : MonoBehaviour
             this.onAdClosed.Invoke(true);
             this.onAdClosed = null;
         }
+        this.adLoaded = false;
         LoadInterstitialAd();
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        this.adLoaded = true;
     }
 
     public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
     {
+        this.adLoaded = false;
         Invoke("LoadInterstitialAd", 60);
     }
 
@@ -97,5 +107,13 @@ public class Ads : MonoBehaviour
     private void SaveAdsEnabled()
     {
         storage.SaveAdsEnabled(this.adsEnabled);
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (!pause)
+        {
+            LoadInterstitialAd();
+        }
     }
 }
