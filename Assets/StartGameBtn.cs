066192870a460376ee1029f27ebff316787a75ab
Fredrick Esposito <fredrickesposito@gmail.com>
2021-08-28 13:03:14 -0400

    Don't show ad when starting new game session

--- Assets/StartGameBtn.cs
+++ Assets/StartGameBtn.cs
@@ -12,16 +12,31 @@ public class StartGameBtn : MonoBehaviour
     public GameObject mainMenu;
     public GameObject newGameConfirmMenu;
     public GameObject removeAdsBtn;
+    private bool pressedBefore = false;
 
     public void StartGame()
     {
-        ads.ShowInterstitialAdAndThen((wasAdShown) => {
-            titleMenu.StartGameAfterDelay(delay);
-            detonator.activate();
-            title.SetActive(false);
-            mainMenu.SetActive(false);
-            newGameConfirmMenu.SetActive(false);
-            removeAdsBtn.SetActive(false);
-        });
+        if(this.pressedBefore)
+        {
+            ads.ShowInterstitialAdAndThen((wasAdShown) => {
+                JustStartGame();
+            });
+        }
+        else
+        {
+            JustStartGame();
+        }
+        
+        this.pressedBefore = true;
+    }
+
+    private void JustStartGame()
+    {
+        titleMenu.StartGameAfterDelay(delay);
+        detonator.activate();
+        title.SetActive(false);
+        mainMenu.SetActive(false);
+        newGameConfirmMenu.SetActive(false);
+        removeAdsBtn.SetActive(false);
     }
 }
