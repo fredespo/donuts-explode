41cb3216e79e3c66306501ee6d83b3e87dc7521a
Fredrick Esposito <fredrickesposito@gmail.com>
2021-09-01 07:19:06 -0400

    Add google play games services auto-login at start

---
+++ Assets/GooglePlayServices.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi;
+
+public class GooglePlayServices : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // authenticate user:
+        PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) => {
+            // handle results
+        });
+    }
+}
