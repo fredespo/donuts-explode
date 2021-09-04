3df6c310925d78cadfb8fe1d707943ab6b11a81a
Fredrick Esposito <fredrickesposito@gmail.com>
2021-09-04 01:36:06 -0400

    Add sign in  / sign out button for google play services

--- Assets/GooglePlayServices.cs
+++ Assets/GooglePlayServices.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using GooglePlayGames;
 using UnityEngine.SocialPlatforms;
 using GooglePlayGames.BasicApi;
 
 public class GooglePlayServices : MonoBehaviour
 {
+    public Text txt;
+    private bool signedIn;
+
     // Start is called before the first frame update
     void Start()
     {
         // authenticate user:
         PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) => {
-            // handle results
+            txt.text = result.ToString();
+            signedIn = result == SignInStatus.Success;
         });
+    }
+
+    public void ShowLeaderboard()
+    {
+        Social.ShowLeaderboardUI();
+    }
+
+    public void SignIn()
+    {
+        PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptAlways, (result) => {
+            txt.text = result.ToString();
+            signedIn = result == SignInStatus.Success;
+        });
+    }
+
+    public void SignOut()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+        signedIn = false;
+    }
+
+    public bool IsSignedIn()
+    {
+        return this.signedIn;
     }
 }
---
+++ Assets/GooglePlayServicesSignInOut.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GooglePlayServicesSignInOut : MonoBehaviour
+{
+    public GooglePlayServices googlePlayServices;
+    public Text buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        if (googlePlayServices.IsSignedIn())
+        {
+            googlePlayServices.SignOut();
+        }
+        else
+        {
+            googlePlayServices.SignIn();
+        }
+    }
+
+    void Update()
+    {
+        if (googlePlayServices.IsSignedIn())
+        {
+            buttonText.text = "Sign out";
+        }
+        else
+        {
+            buttonText.text = "Sign in";
+        }
+    }
+}
