93a037de1a19b784457b3f7dcf493e0d80129084
Fredrick Esposito <fredrickesposito@gmail.com>
2021-09-13 23:32:05 -0400

    Adjust min and max rotation speed to 120 - 275

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -20,8 +20,8 @@ public class Rotator : MonoBehaviour
 
     void Update()
     {
-        float min = 140f;
-        float max = 400f;
+        float min = 120f;
+        float max = 275f;
         float elapsedRatio = this.timer.GetTimeElapsed() / this.timer.GetStartSeconds();
         float degPerSec = min + ((max - min) * elapsedRatio);
         transform.Rotate(0, 0, degPerSec * Time.deltaTime * (direction == RotationDir.Clockwise ? -1 : 1));
