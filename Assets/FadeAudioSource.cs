6bb07bafce783ddb2d750f5c41391fcfe31c67c9
Fredrick Esposito <fredrickesposito@gmail.com>
2021-09-18 23:14:03 -0400

    Fade music to start

---
+++ Assets/FadeAudioSource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAudioSource : MonoBehaviour
+{
+    public float duration;
+    public float targetVolume;
+
+    public void StartFadeCoroutine()
+    {
+        StartCoroutine("StartFade");
+    }
+
+    public IEnumerator StartFade()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        float currentTime = 0;
+        float start = audioSource.volume;
+
+        while (currentTime < this.duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, this.targetVolume, currentTime / this.duration);
+            yield return null;
+        }
+        yield break;
+    }
+}
