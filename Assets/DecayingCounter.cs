6b30fdce34db997ca383f0a9bd85b01b77ed070b
Fredrick Esposito <fredrickesposito@gmail.com>
2021-10-31 17:47:45 -0400

    Hide level select unless you tap 5 times on bottom of title screen

---
+++ Assets/DecayingCounter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DecayingCounter : MonoBehaviour
+{
+    public int count = 0;
+    public int targetCount = 5;
+    public float resetAfterSeconds = 1f;
+    public UnityEvent onTargetReached;
+
+    void Start()
+    {
+        
+    }
+
+    public void IncrementCount()
+    {
+        if(this.count == 0)
+        {
+            StartCoroutine(ResetCoroutine());
+        }
+        this.count++;
+        if(this.count == this.targetCount)
+        {
+            this.onTargetReached.Invoke();
+            ResetCount();
+        }
+    }
+
+    private void ResetCount()
+    {
+        this.count = 0;
+    }
+
+    private IEnumerator ResetCoroutine()
+    {
+        yield return new WaitForSeconds(this.resetAfterSeconds);
+        ResetCount();
+    }
+}
