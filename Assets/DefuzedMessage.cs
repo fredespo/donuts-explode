e179d74b79dd3117fe4c886b3036181baa8b205a
Fredrick Esposito <fredrickesposito@gmail.com>
2021-11-10 19:40:53 -0500

    Make defuzed message fade from red to black

---
+++ Assets/DefuzedMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefuzedMessage : MonoBehaviour
+{
+    public AudioSource extinguishSound;
+
+    public void PlayExtinguishSound()
+    {
+        this.extinguishSound.Play();
+    }
+}
