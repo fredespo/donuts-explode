e2358c05428a6ebab0c775f82ef58bc709313712
Fredrick Esposito <fredrickesposito@gmail.com>
2021-12-02 22:14:48 -0500

    Add basic animation to bonus level win UI

---
+++ Assets/BonusLevelWinUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusLevelWinUI : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject buttons;
+
+    public void Reveal()
+    {
+        this.anim.SetTrigger("show");
+    }
+
+    public void AwardBonusPoints()
+    {
+        Debug.Log("Awarding bonus points...");
+        StartCoroutine(ShowButtonsAfterDelay(2.0f));
+    }
+
+    private IEnumerator ShowButtonsAfterDelay(float delaySec)
+    {
+        yield return new WaitForSeconds(delaySec);
+        this.buttons.SetActive(true);
+    }
+}
