3ed4b73d8aeb9a78db685de162224530b06d3e94
Fredrick Esposito <fredrickesposito@gmail.com>
2021-12-29 02:55:12 -0500

    Add sound effect to bonus level indicator
    Also fix bug whereby the metal impact sound effect would not play for bonus level win UI beyond the first shown.

---
+++ Assets/BonusLevelIndicator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusLevelIndicator : MonoBehaviour
+{
+    private AudioSource soundEffect;
+
+    void Start()
+    {
+        this.soundEffect = GetComponent<AudioSource>();
+    }
+
+    public void PlaySoundEffect()
+    {
+        this.soundEffect.Play();
+    }
+}
