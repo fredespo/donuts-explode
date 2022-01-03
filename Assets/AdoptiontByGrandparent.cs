8009d1f96a59fbf46b58619ef4f83d765f8cc778
Fredrick Esposito <fredrickesposito@gmail.com>
2022-01-03 03:11:19 -0500

    Fix bonus bomb explosion sound
    It would get cut off before finishing since the explosion animation would finish sooner than the sound effect.

---
+++ Assets/AdoptiontByGrandparent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdoptiontByGrandparent : MonoBehaviour
+{
+    public int generationsToGoBackFromGrandparent;
+    public float delay;
+
+    void Start()
+    {
+        StartCoroutine(GetAdoptedByGrandparentsCoroutine(this.delay, this.generationsToGoBackFromGrandparent));      
+    }
+
+    private IEnumerator GetAdoptedByGrandparentsCoroutine(float delay, int generationsToGoBackFromGrandparent)
+    {
+        yield return new WaitForSeconds(delay);
+        GetAdoptedByGrandparents(generationsToGoBackFromGrandparent);
+    }
+
+    private void GetAdoptedByGrandparents(int generationsToGoBackFromGrandparent)
+    {
+        Transform newParent = this.transform.parent.parent.transform;
+        for(int i = 0; i < generationsToGoBackFromGrandparent; ++i)
+        {
+            newParent = newParent.transform.parent.transform;
+        }
+        this.transform.parent = newParent;
+    }
+}
---
+++ Assets/AutoSelfDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSelfDestroyer : MonoBehaviour
+{
+    public float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestorySelfCoroutine());
+    }
+
+    private IEnumerator DestorySelfCoroutine()
+    {
+        yield return new WaitForSeconds(this.delay);
+        Destroy(gameObject);
+    }
+}
