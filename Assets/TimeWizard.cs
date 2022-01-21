7ee0ef591710e4c6506be411c2d7facda53ea87d
Fredrick Esposito <fredrickesposito@gmail.com>
2022-01-21 03:13:13 -0500

    Make Save Score btn not exit game won screen

---
+++ Assets/TimeWizard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeWizard : MonoBehaviour
+{
+    public void SetTimeScale(float value)
+    {
+        Time.timeScale = value;
+    }
+}
