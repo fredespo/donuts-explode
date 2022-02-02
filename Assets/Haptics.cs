35024f1d6bc5271a0880e948e41b43d6ee841a14
Fredrick Esposito <fredrickesposito@gmail.com>
2022-02-02 04:27:40 -0500

    Import Nice Vibrations package
    Also add some temporary buttons on the settings screen to test haptics.

---
+++ Assets/Haptics.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Lofelt.NiceVibrations;
+
+public class Haptics : MonoBehaviour
+{
+    public void Vibrate()
+    {
+        Handheld.Vibrate();
+    }
+
+    public void PlayEmphasis()
+    {
+        HapticPatterns.PlayEmphasis(1.0f, 0.5f);
+    }
+
+    public void PlayConstant()
+    {
+        HapticPatterns.PlayConstant(1.0f, 0.5f, 1.0f);
+    }
+
+    public void PlayWarningPreset()
+    {
+        HapticPatterns.PlayPreset(HapticPatterns.PresetType.Warning);
+    }
+}
