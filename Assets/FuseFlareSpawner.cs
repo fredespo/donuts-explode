80685fb447967b94e6e94f77b96a18809cff2da1
Fredrick Esposito <fredrickesposito@gmail.com>
2022-02-09 02:54:43 -0500

    Revert "Make fuse flare effect frame rate independent"
    This reverts commit 965788b0cccc0ad4dc763f555bbc965f2eaff55a.

--- Assets/FuseFlareSpawner.cs
+++ Assets/FuseFlareSpawner.cs
@@ -7,27 +7,20 @@ public class FuseFlareSpawner : MonoBehaviour
     public GameObject flare;
     public Vector2 spawnDistanceRange;
     public Vector2 rotationRange;
-    public float spawnsPerSecond = 60;
-    public int numConcurrent = 20;
-    private float numToSpawn= 0;
+    public float spawnDelay = 1.0f;
+    private float spawnTimer = 0.0f;
 
     void Update()
     {
-        this.numToSpawn += this.spawnsPerSecond * Time.deltaTime;
-        int numToSpawnThisFrame = (int)this.numToSpawn;
-        if (numToSpawnThisFrame > 0)
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer >= spawnDelay)
         {
-            this.numToSpawn %= numToSpawnThisFrame;
-        }
-
-        for(int i = 0; i < numToSpawnThisFrame; ++i)
-        {
-            FuseFlare spawnedFlare = SpawnFlare();
-            spawnedFlare.fadeSpeed = 1 / (Time.deltaTime * this.numConcurrent);
+            SpawnFlare();
+            spawnTimer = 0.0f;
         }
     }
 
-    FuseFlare SpawnFlare()
+    void SpawnFlare()
     {
         GameObject spawnedFlare = Instantiate(flare);
         spawnedFlare.transform.SetParent(gameObject.transform);
@@ -43,6 +36,5 @@ public class FuseFlareSpawner : MonoBehaviour
             gameObject.transform.localRotation.y,
             degrees
         );
-        return spawnedFlare.GetComponent<FuseFlare>();
     }
 }
