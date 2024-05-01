c7baf9e1fc78af22245aeea234e4590f22f4d30f
Fredrick Esposito <fredrickesposito@gmail.com>
2024-05-01 16:28:07 -0400

    Add donut variations
    Also adds a way to switch between them on level 1 (for testing purposes): arrow keys on desktop, shaking on mobile.

---
+++ Assets/DonutPaletteProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BinaryCharm.SemanticColorPalette;
+
+public class DonutPaletteProvider : MonoBehaviour
+{
+    private SCP_PaletteProvider paletteProvider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.paletteProvider = GetComponent<SCP_PaletteProvider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // on mobile if you shake the device then the palette will change
+        // or if the device is not mobile then you can use the arrow keys to change the palette
+        if (Input.acceleration.sqrMagnitude > 10 || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangePalette(Input.GetKeyDown(KeyCode.LeftArrow) ? -1 : 1);
+        }
+    }
+
+    private void ChangePalette(int direction = 1)
+    {
+        int nextPaletteIndex = this.paletteProvider.GetActivePaletteIndex() + direction;
+        if (nextPaletteIndex >= this.paletteProvider.GetNumPalettes())
+        {
+            nextPaletteIndex = 0;
+        }
+        else if (nextPaletteIndex < 0)
+        {
+            nextPaletteIndex = this.paletteProvider.GetNumPalettes() - 1;
+        }
+        this.paletteProvider.SetActivePaletteIndex(nextPaletteIndex);
+    }
+}
