5b692f97dc889404c5a6d00215dc5b650dd5ef32
Fredrick Esposito <fredrickesposito@gmail.com>
2024-05-08 10:44:53 -0400

    Set random donut palette
    Also remove ability to change donut palette with keys or swipe (for now, could easily put back in for testing purposes).

--- Assets/DonutPaletteProvider.cs
+++ Assets/DonutPaletteProvider.cs
@@ -5,41 +5,55 @@ using BinaryCharm.SemanticColorPalette;
 
 public class DonutPaletteProvider : MonoBehaviour
 {
+    public boolean randomizePaletteOnStart = true;
     private SCP_PaletteProvider paletteProvider;
-    private Vector2 startPos;
-    private float minSwipeDist = 50f;
+    private Vector2 touchStart;
 
-    // Start is called before the first frame update
     void Start()
     {
         this.paletteProvider = GetComponent<SCP_PaletteProvider>();
+        if (this.randomizePaletteOnStart) changePaletteToRandom();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Check for swipe gestures on mobile
+        // changePaletteWithSwipe(50f);
+        // changePaletteWithKeys();
+    }
+
+    private void changePaletteToRandom()
+    {
+        this.paletteProvider.SetActivePaletteIndex(
+            Random.Range(0, this.paletteProvider.GetNumPalettes())
+        );
+    }
+
+    private void changePaletteWithKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangePalette(Input.GetKeyDown(KeyCode.LeftArrow) ? -1 : 1);
+        }
+    }
+
+    private void changePaletteWithSwipe(float minSwipeDist)
+    {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
-                startPos = touch.position;
+                touchStart = touch.position;
             }
             else if (touch.phase == TouchPhase.Ended)
             {
-                float swipeDist = (touch.position - startPos).magnitude;
-                if (swipeDist > minSwipeDist) // minSwipeDist is a threshold for minimum swipe distance
+                float swipeDist = (touch.position - touchStart).magnitude;
+                if (swipeDist > minSwipeDist)
                 {
                     float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
                     ChangePalette(swipeValue > 0 ? 1 : -1);
                 }
             }
-        }
-        // For non-mobile platforms, use the arrow keys to change the palette
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            ChangePalette(Input.GetKeyDown(KeyCode.LeftArrow) ? -1 : 1);
         }
     }
 
