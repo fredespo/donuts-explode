c8b485b1daddab3bed2db7e6485d780ec80be066
Fredrick Esposito <fredrickesposito@gmail.com>
2024-10-08 18:04:58 -0400

    Make game won donuts more spread out

--- Assets/DonutSpawner.cs
+++ Assets/DonutSpawner.cs
@@ -7,6 +7,8 @@ public class DonutSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject donut;
     [SerializeField] private SCP_PaletteProvider donutPaletteProvider;
+    public float minDistX = 300;
+    private float prevX = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +20,17 @@ public class DonutSpawner : MonoBehaviour
     void SpawnDonut()
     {
         GameObject spawnedDonut = Instantiate(this.donut, gameObject.transform);
-        spawnedDonut.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(-400, 400), 300);
+        spawnedDonut.GetComponent<RectTransform>().anchoredPosition = new Vector2(getNextDonutSpawnX(), 300);
         spawnedDonut.GetComponent<Bomb>().SetPalette(UnityEngine.Random.Range(0, this.donutPaletteProvider.GetNumPalettes()));
+    }
+
+    private float getNextDonutSpawnX() {
+        float xPos = Random.Range(-400, 400);
+        if (Mathf.Abs(xPos - this.prevX) < this.minDistX) {
+            xPos += (this.prevX < 0 ? this.minDistX : -this.minDistX);
+        }
+        this.prevX = xPos;
+        return xPos;
     }
 
     void ClearDonuts() {
